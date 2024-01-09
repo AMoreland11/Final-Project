e18ab2203a5e4d5b681bb7a6f34b2218cc9e9f91
AMoreland11 <a11daaron@gmail.com>
2024-01-09 15:32:51 -0500

    trying to do slide animation

---
+++ app.js
@@ -0,0 +1,38 @@
+// function slideLeft() {
+//     document.body.classlist += "slide-left"
+// }
+
+
+
+// function slideRight() {
+//     document.body.classList += "slide-right"
+// }
+
+const scrollOffset = 60;
+const sections = document.querySelectorAll(".animate-section");
+
+const sectionInView = (section, offset = 0) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    return (
+        sectionTop <=
+        (window.innerHeight || document.documentElement.clientHeight) - offset
+    );
+};
+
+
+const handleScrollAnimation = () => {
+    sections.forEach(section => {
+        if (sectionInView(section, scrollOffset)) {
+            section.classList.add("scrolled");
+        } else {
+            section.classList.remove("scrolled");
+        }
+    });
+};
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
+
+
+
